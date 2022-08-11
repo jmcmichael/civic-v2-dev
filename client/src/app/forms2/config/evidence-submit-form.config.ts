56ad08ecdf42b0e7c4352b5de563f0dcd43aa5d0
Joshua McMichael <jmcmicha@wustl.edu>
2022-08-11 07:37:14 -0500

    base form config set up

---
+++ client/src/app/forms2/config/evidence-submit-form.config.ts
@@ -0,0 +1,16 @@
+export const evidenceSubmitFields = [{
+    key: 'fields',
+    wrappers: ['form-container'],
+    templateOptions: {
+        label: "Submit an Evidence Item"
+    },
+    fieldGroup: [
+        {
+            key: 'geneId',
+            type: 'input',
+            templateOptions: {
+                label: 'Gene'
+            }
+        }
+    ]
+}]
--- client/src/app/forms2/forms2.config.ts
+++ client/src/app/forms2/forms2.config.ts
@@ -1,8 +1,5 @@
 import { ConfigOption } from '@ngx-formly/core';
-// import { defaultMessages, defaultValidators } from './config/validators/default.validators';
 
 export const CvcFormlyConfig2: ConfigOption = {
   extras: { immutable: true },
-  // validationMessages: defaultMessages,
-  // validators: defaultValidators,
 }
