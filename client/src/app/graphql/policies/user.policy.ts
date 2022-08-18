b613d5567101d814e1a4b664ebb9239fb5ced42d
Joshua McMichael <jmcmicha@wustl.edu>
2022-08-18 09:44:50 -0500

    added some links to graphql, codegen docs

--- client/src/app/graphql/policies/user.policy.ts
+++ client/src/app/graphql/policies/user.policy.ts
@@ -1,9 +1,11 @@
+// define behavior for User fields
+// see: https://www.apollographql.com/docs/react/caching/cache-field-behavior/
 import { Reference, TypePolicy } from '@apollo/client/cache'
 import { Maybe } from '@app/generated/civic.apollo'
 export const CvcUserPolicy: TypePolicy = {
   fields: {
     mostRecentOrg: {
-      read: (_: Maybe<Reference>, { readField }) => {
+      read: (_: Maybe<Reference>, { readField }): Maybe<Reference> => {
         // return reference to most recently used Organization
         const mroId: Maybe<number> = readField('mostRecentOrganizationId')
         const orgs: Maybe<readonly Reference[]>= readField('organizations')
