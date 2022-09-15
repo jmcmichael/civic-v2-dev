011dcbcaa0a494a09060ceafd843dfbb1f5d54a9
Joshua McMichael <jmcmicha@wustl.edu>
2022-09-15 12:15:57 -0500

    moved all of base-input's tag display code into mixin

---
+++ client/src/app/forms2/mixins/display-string-tag.mixin.ts
@@ -0,0 +1,81 @@
+import { Injectable } from '@angular/core'
+import { Maybe } from '@app/generated/civic.apollo'
+import { untilDestroyed } from '@ngneat/until-destroy'
+import { FieldType } from '@ngx-formly/core'
+import { Subject } from 'rxjs'
+import { tag } from 'rxjs-spy/operators'
+import { MixinConstructor } from 'ts-mixin-extended'
+
+export function DisplayStringTag<TBase extends MixinConstructor<FieldType>>(
+  Base: TBase
+) {
+  @Injectable()
+  abstract class DisplayStringTagMixin extends Base {
+    // BASE FIELD TYPE SOURCE STREAMS
+    onValueChange$?: Subject<Maybe<string | number>>
+
+    // LOCAL SOURCE STREAMS
+    onTagClose$!: Subject<MouseEvent> // emits on entity tag closed btn click
+
+    // LOCAL PRESENTATION STREAMS
+    tagLabel$!: Subject<Maybe<string>> // emits label for tag
+
+    configureDisplayStringTag(): void {
+      if (!this.onValueChange$) {
+        console.error(
+          `${this.field.id} cannot find onValueChange$ Subject, ensure configureBaseField() has been called before configureDisplayStringTag in its AfterViewInit hook.`
+        )
+        return
+      }
+
+      this.tagLabel$ = new Subject<Maybe<string>>()
+      this.onValueChange$.pipe(tag(`${this.field.id} onValueChanges$`), untilDestroyed(this)).subscribe((str: Maybe<string | number>) => {
+        this.tagLabel$.next(str ? str.toString() : undefined)
+      })
+
+      this.onTagClose$ = new Subject<MouseEvent>()
+
+      if (this.props.isRepeatItem) {
+        // check if parent field is of 'repeat-field' type
+        if (
+          !(this.field.parent && this.field.parent?.type === 'repeat-field')
+        ) {
+          console.error(
+            `${this.field.id} does not appear to have a parent type of 'repeat-field'.`
+          )
+        } else {
+          // check if parent repeat-field attached the onRemove$ Subject
+          if (!this.field.parent?.props?.onRemove$) {
+            console.error(
+              `${this.field.id} cannot find reference to parent repeat-field onRemove$.`
+            )
+          } else {
+            const onRemove$: Subject<number> = this.field.parent.props.onRemove$
+            this.onTagClose$.pipe(untilDestroyed(this)).subscribe((_) => {
+              this.resetField()
+              onRemove$.next(Number(this.key))
+            })
+          }
+        }
+      } else {
+        this.onTagClose$.pipe(untilDestroyed(this)).subscribe((_) => {
+          this.resetField()
+        })
+      }
+    }
+    unsetModel() {
+      this.formControl.setValue(undefined)
+    }
+
+    deleteTag() {
+      this.tagLabel$.next(undefined)
+    }
+
+    resetField() {
+      this.unsetModel()
+      this.deleteTag()
+    }
+  }
+
+  return DisplayStringTagMixin
+}
--- client/src/app/forms2/types/base-input/base-input.component.ts
+++ client/src/app/forms2/types/base-input/base-input.component.ts
@@ -8,13 +8,12 @@ import {
 import { BaseFieldType } from '@app/forms2/mixins/base/field-type-base'
 import { DisplayStringTag } from '@app/forms2/mixins/display-string-tag.mixin'
 import { Maybe } from '@app/generated/civic.apollo'
-import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy'
+import { UntilDestroy } from '@ngneat/until-destroy'
 import {
   FieldTypeConfig,
   FormlyFieldConfig,
   FormlyFieldProps,
 } from '@ngx-formly/core'
-import { Subject } from 'rxjs'
 import mixin from 'ts-mixin-extended'
 
 export interface CvcBaseInputFieldProps extends FormlyFieldProps {
@@ -27,11 +26,13 @@ export interface CvcBaseInputFieldConfig
 }
 
 const BaseInputMixin = mixin(
-  BaseFieldType<FieldTypeConfig<CvcBaseInputFieldProps>, Maybe<string|number>>(),
+  BaseFieldType<
+    FieldTypeConfig<CvcBaseInputFieldProps>,
+    Maybe<string | number>
+  >(),
   DisplayStringTag
 )
 
-@UntilDestroy()
 @Component({
   selector: 'cvc-base-input',
   templateUrl: './base-input.component.html',
@@ -39,12 +40,6 @@ const BaseInputMixin = mixin(
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CvcBaseInputField extends BaseInputMixin implements AfterViewInit {
-  // SOURCE STREAMS
-  onTagClose$: Subject<MouseEvent> // emits on entity tag closed btn click
-
-  // PRESENTATION STREAMS
-  tagLabel$: Subject<Maybe<string>> // emits label for tag
-
   defaultOptions: Partial<FieldTypeConfig<CvcBaseInputFieldProps>> = {
     modelOptions: {
       updateOn: 'blur', // update model when focus leaves field (see enter keydown.enter EventEmitter in template)
@@ -55,61 +50,12 @@ export class CvcBaseInputField extends BaseInputMixin implements AfterViewInit {
     },
   }
 
-  repeatFieldId?: string
-
   constructor(public injector: Injector) {
     super(injector)
-    this.onTagClose$ = new Subject<MouseEvent>()
-    this.tagLabel$ = new Subject<Maybe<string>>()
   }
 
   ngAfterViewInit(): void {
     this.configureBaseField()
     this.configureDisplayStringTag()
-
-    this.onValueChange$.subscribe((str: Maybe<string | number>) => {
-      this.tagLabel$.next(str ? str.toString() : undefined)
-    })
-
-    // if this is a repeat-item field, emit onRemove$ event on tag close,
-    // otherwise, just reset field locally
-    if (this.props.isRepeatItem) {
-      // check if parent field is of 'repeat-field' type
-      if (!(this.field.parent && this.field.parent?.type === 'repeat-field')) {
-        console.error(
-          `${this.field.id} does not appear to have a parent type of 'repeat-field'.`
-        )
-      } else {
-        // check if parent repeat-field attached the onRemove$ Subject
-        if (!this.field.parent?.props?.onRemove$) {
-          console.error(
-            `${this.field.id} cannot find reference to parent repeat-field onRemove$.`
-          )
-        } else {
-          const onRemove$: Subject<number> = this.field.parent.props.onRemove$
-          this.onTagClose$.pipe(untilDestroyed(this)).subscribe((_) => {
-            this.resetField()
-            onRemove$.next(Number(this.key))
-          })
-        }
-      }
-    } else {
-      this.onTagClose$.pipe(untilDestroyed(this)).subscribe((_) => {
-        this.resetField()
-      })
-    }
   } // ngAfterViewInit
-
-  unsetModel() {
-    this.formControl.setValue(undefined)
-  }
-
-  deleteTag() {
-    this.tagLabel$.next(undefined)
-  }
-
-  resetField() {
-    this.unsetModel()
-    this.deleteTag()
-  }
 }
