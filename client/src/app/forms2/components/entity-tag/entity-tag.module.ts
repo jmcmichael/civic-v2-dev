7d16db3afc5163c9c236ba5484d2787e1b3dc961
Joshua McMichael <jmcmicha@wustl.edu>
2022-10-09 12:48:23 -0500

    entity-tags working with tooltips in closeable mode

--- client/src/app/forms2/components/entity-tag/entity-tag.module.ts
+++ client/src/app/forms2/components/entity-tag/entity-tag.module.ts
@@ -4,10 +4,17 @@ import { CvcEntityTagComponent } from './entity-tag.component'
 import { NzIconModule } from 'ng-zorro-antd/icon'
 import { NzTagModule } from 'ng-zorro-antd/tag'
 import { CvcPipesModule } from '@app/core/pipes/pipes.module'
+import { NzToolTipModule } from 'ng-zorro-antd/tooltip'
 
 @NgModule({
   declarations: [CvcEntityTagComponent],
-  imports: [CommonModule, NzIconModule, NzTagModule, CvcPipesModule],
+  imports: [
+    CommonModule,
+    NzIconModule,
+    NzTagModule,
+    NzToolTipModule,
+    CvcPipesModule,
+  ],
   exports: [CvcEntityTagComponent],
 })
 export class CvcEntityTagModule {}
