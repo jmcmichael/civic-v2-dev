9dd93d39f5b460d1f056f347b0f249163b160bca
Joshua McMichael <jmcmicha@wustl.edu>
2022-11-10 10:54:40 -0600

    baseline grid config working

--- client/src/app/forms2/config/evidence-submit/evidence-submit.form.config.ts
+++ client/src/app/forms2/config/evidence-submit/evidence-submit.form.config.ts
@@ -73,8 +73,10 @@ const formFieldConfig: FormlyFieldConfig[] = [
           },
           {
             wrappers: ['field-group'],
-            props: {
-              layout: 'inline'
+            props: <CvcFieldGroupWrapperConfig>{
+              grid: {
+                layout: 'grid',
+              },
             },
             fieldGroup: [
               <CvcDrugSelectFieldOptions>{
